5126b369d48556711a13f5f7bfb4eff8c0e44109
Chris_Wallace <kawallis@icloud.com>
2017-04-17 16:44:12 -0700

    changes

--- public/app.js
+++ public/app.js
@@ -1,9 +1,8 @@
 'use strict';
-
-
+// grabs the audio HTML element
+var song = document.getElementById('audioElement');
 var button = document.getElementsByTagName('button');
 button[0].addEventListener('click', function (event) {
-
   init();
 });
 
@@ -14,16 +13,11 @@ if(!AUDIO) console.error('Web Audio API not supported :(');
 // Create and configure analyzer node and storage buffer
 var analyzer = AUDIO.createAnalyser();
 analyzer.fftSize = 128;
-
 var bufferLength = analyzer.frequencyBinCount;
 
 //tried to wrap array to fix error
 var ab = new ArrayBuffer(bufferLength);
 var dataArray = new Uint8Array(ab);
-console.log(dataArray);
-// grabs the audio HTML element
-var song = document.getElementById('audioElement');
-
 
 function init() {
   // Connect song to analyzer and analyzer to audio-out
@@ -33,41 +27,34 @@ function init() {
   loop();
 }
 
-
-
 function loop() {
   requestAnimationFrame(loop);
   analyzer.getByteFrequencyData(dataArray);
-  // console.log(dataArray);
-  console.log(dataArray);
-  // var newArr = dataArray.slice(43);
   song.play();
   myChart.data.datasets[0].data = dataArray;
-  // updateChart();
   myChart.update();
 }
 
 //creates the initial chartjs
-var colorArr = [];
 var initNumArr = [];
+var labelsArr = [];
 for(var i = 0; i < 64; i++){
-  colorArr.push('rgb(255, 255, 0)');
   initNumArr.push(0);
+  labelsArr.push('');
 }
-console.log(colorArr);
 var ctx = document.getElementById('myChart');
 var myChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ['', '', '', '', '','', '', '', '', '', '', '', '', '', '','', '', '', '', '', ''],
+    labels: labelsArr,
     datasets: [{
       label: '',
       data: initNumArr,
-      backgroundColor: colorArr,
-      borderColor: colorArr,
+      backgroundColor: 'rgb(255, 255, 0)',
+      borderColor: 'rgb(255, 255, 0)',
       borderWidth: 0
     }]
-  },
+  },  
   options: {
     scales: {
       xAxes: [{
@@ -88,4 +75,3 @@ var myChart = new Chart(ctx, {
     }
   }
 });
-
